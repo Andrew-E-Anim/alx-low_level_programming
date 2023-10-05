b2d223df4c7e1a8542ca1d476f00eff8e554c123
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-05 12:42:31 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 int main(void)
 {
 	/*
-	 * The main functions allows C to be able to print 
+	 * The main function allows C to be able to print 
 	 * the text
 	 */
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
-
