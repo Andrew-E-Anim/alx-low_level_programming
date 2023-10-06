6ba8db063775e92229b38eec7ef02119191d5089
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 15:43:01 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- * description: print lowercase and uppercase alphabets
- * return: 0 success
+ * main - Entry point
+ * Description: print lowercase and uppercase alphabets
+ * Return: 0 success
  */
 int main(void)
 {
