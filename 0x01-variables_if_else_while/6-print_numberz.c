60df9fab33d987454f86d5e14d152e818c9dd45a
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 16:15:10 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ * Description print 0 to 9
+ * Return : 0 
+ */
+
+int main (void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar((shar)num)
+	putchar('\n');
+	return (0);
+}
