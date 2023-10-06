9be4357e537dc72362c344f83a9459aeef40caf3
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 16:25:50 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  * Description: print number 0 to 9
- * Return: Always 0 (success)
+ * Return: 0 (success)
  */
 
 int main(void)
 {
 	int num;
 
-		for (num = 0; number <=9; num++)
+	for (num = 0; num <= 9; num++)
 			print f("%d", num);
 	printf("\n");
 	return (0);
