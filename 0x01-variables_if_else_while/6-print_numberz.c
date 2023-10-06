5e1f3796dae818628dcc7f39256306607c383d50
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 16:19:29 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - Entry point
- * Description print 0 to 9
- * Return : 0 
+ * Description: print number 0 to 9
+ * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	int num;
 
-	for (num = 48; num <= 57; num++)
-		putchar((shar)num)
-	putchar('\n');
+		for (num = 0; number <=9; num++)
+			print f("%d", num);
+	printf("\n");
 	return (0);
 }
+
