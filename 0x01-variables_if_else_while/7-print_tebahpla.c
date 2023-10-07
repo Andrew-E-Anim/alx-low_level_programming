5648d962672fd9e95d42e8ab750fe17a631877ea
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 17:01:12 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,15 +2,16 @@
 
 /**
  * main - Entry point
- * Description: print number 0 to 9
+ * Description: prints the lowercase in reverse
  * Return: 0 (success)
  */
+
 int main(void)
 {
-	int num;
-
-	for (num = 0; num <= 9 ; num++)
-		putchar((char)num);
+	char ch;
+	
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
