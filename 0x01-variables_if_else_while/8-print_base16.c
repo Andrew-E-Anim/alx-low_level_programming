40f46d49249a505978f6fd763fe2a55abfcdeef5
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-06 17:13:22 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  * Description: prints all numbers of base 16 in lowercase
- * Return 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	char ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (ch = '0'; ch <= '10'; ch++)
 		putchar(ch);
 	for (ch = 'a'; ch < 'f'; ch++)
 		putchar(ch);
