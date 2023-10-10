089d65a0ff521fa070a832c60d7e27ee65b3f1b8
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-10 15:47:07 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,7 +6,7 @@
  *
  * Return: num1 + num2
  */
-int add(int num1,int num2)
+int add(int num1, int num2)
 {
 	return (num1 + num2);
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: represents natural number
+ */
+
+void print_to_98(int n)
+{
+	while (n != 9)
+	{
+		printf("%d", n)
+			if (n > 98)
+				--n;
+			else if (n < 98)
+				++n;
+		printf(", ");
+	}
+	printf("%d", 98);
+	printf("\n");
+}
