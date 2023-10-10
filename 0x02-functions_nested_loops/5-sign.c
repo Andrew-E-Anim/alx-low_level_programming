d9621d9126e0668a25b9e3056bf0ba19aa100421
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-10 14:32:12 -0700

    Print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar('-');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
