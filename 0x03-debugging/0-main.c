8664f6aff479cf37bfd595613c278e26582de5d2
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-12 06:54:54 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop: (\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
