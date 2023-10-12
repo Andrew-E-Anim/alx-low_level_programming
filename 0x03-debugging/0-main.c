15654342021b4adaef627701e5d0b9461403f266
Andrew-E-Anim <andrew.eanim@gmail.com>
2023-10-12 07:31:53 -0700

    Tests in integer is positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - test for positive or negative integer
- * Return: 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 int main(void)
 {
